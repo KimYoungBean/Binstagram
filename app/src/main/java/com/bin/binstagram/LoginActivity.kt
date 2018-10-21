a6c2b3d2573b12296a38525e09ed51b3aae6392e
Kim YoungBean <whiteyb12@gmail.com>
2018-10-21 23:54:24 +0900

    email login

--- app/src/main/java/com/bin/binstagram/LoginActivity.kt
+++ app/src/main/java/com/bin/binstagram/LoginActivity.kt
@@ -1,12 +1,55 @@
 package com.bin.binstagram
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            createAndLoginEmail()
+        }
+
+
+    }
+
+    fun createAndLoginEmail(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())?.addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                moveMainPage(auth?.currentUser)
+            }else if(task.exception?.message.isNullOrEmpty()){
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }else{
+
+            }
+        }
+    }
+
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+                ?.addOnCompleteListener {   task ->
+                    if(task.isSuccessful){
+                        moveMainPage(auth?.currentUser)
+                    }else{
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+        }
+    }
+
+    fun moveMainPage(user :FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
